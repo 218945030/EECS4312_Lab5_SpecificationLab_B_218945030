5927353e3d2717d556f45d043e83e769256842c5
Jonah O <jonah21@my.yorku.ca>
2026-02-02 18:18:22 -0500

    Edited test case

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -69,6 +69,6 @@ def test4 ():
 
 def test5 ():
     resources = {'gpu': 5}
-    requests = [{'gpu': 3}, ['mem', 1]]  # malformed request
+    requests = [{'gpu': 3}, ['mem', 1]]
     with pytest.raises(ValueError):
         is_allocation_feasible(resources, requests)
